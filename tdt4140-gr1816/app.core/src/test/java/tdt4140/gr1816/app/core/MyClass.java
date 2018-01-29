7d4a8e8af58d18015352095d31560f796ad3c68d
Odin Ugedal <odin@ugedal.com>
2018-01-29 11:25:03 +0100

    Fix test

--- tdt4140-gr1816/app.core/src/test/java/tdt4140/gr1816/app/core/MyClass.java
+++
@@ -1,8 +0,0 @@
-package tdt4140.gr1816.app.core;
-
-public class MyClass {
-	
-	double multiply(double A, double B){
-		return A*B;
-	}
-}
--- tdt4140-gr1816/app.core/src/test/java/tdt4140/gr1816/app/core/MyUnitTest.java
+++ tdt4140-gr1816/app.core/src/test/java/tdt4140/gr1816/app/core/MyUnitTest.java
@@ -1,11 +1,27 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
+class MyClass {
+	
+	public static double multiply(double A, double B){
+		return A*B;
+	}
+}
 
 public class MyUnitTest {
-
     @Test
     public void testConcatenate() {
         assertEquals("onetwo", "onetwo");
 
+    }
+    @Test
+    public void multiplicationOfZeroIntegersShouldReturnZero() {
+        MyClass tester = new MyClass(); // MyClass is tested
+
+        // assert statements
+        assertEquals(0.0, tester.multiply(10, 0), 0.001);
+        assertEquals(0.0, tester.multiply(0, 10), 0.001);
+        assertEquals(0.0, tester.multiply(0, 0), 0.001);
     }
 }
