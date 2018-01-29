9881178f111d82a80149139f875532fcb7688a30
Odin Ugedal <odin@ugedal.com>
2018-01-29 10:53:58 +0100

    Add simple test

---
+++ tdt4140-gr1816/app.core/src/test/java/tdt4140/gr1816/app/core/MyUnitTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MyUnitTest {
+
+    @Test
+    public void testConcatenate() {
+        assertEquals("onetwo", "onetwo");
+
+    }
+}
