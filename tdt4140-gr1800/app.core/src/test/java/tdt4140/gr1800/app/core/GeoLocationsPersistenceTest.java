b929f8425192ad78a922d2dfceccf44880ef12f0
Mathias Wahl <ma10asw@gmail.com>
2018-02-01 14:50:36 +0100

    Fix #2, reboot entire project
    Update 18nn that was missed
    
    Add JUnit test and Readme with Roles
    
    Delete .project files

--- tdt4140-gr1800/app.core/src/test/java/tdt4140/gr1800/app/core/GeoLocationsPersistenceTest.java
+++ tdt4140-gr1800/app.core/src/test/java/tdt4140/gr1800/app/core/GeoLocationsPersistenceTest.java
@@ -2,8 +2,8 @@ package tdt4140.gr1800.app.core;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
 
@@ -24,31 +24,37 @@ public class GeoLocationsPersistenceTest {
 	
 	@Test
 	public void testLoadLocations() {
-		testLoadLocations(getClass().getResourceAsStream("geoLocations.json"));
-	}
-	
-	private void testLoadLocations(InputStream inputStream) {
 		try {
-			Collection<GeoLocations> geoLocations = persistence.loadLocations(inputStream);
-			Assert.assertEquals(2, geoLocations.size());
-			Iterator<GeoLocations> it = geoLocations.iterator();
-			GeoLocationsTest.assertGeoLocations(it.next(), new LatLong(63, 10), new LatLong(63.1, 10.1));
-			GeoLocationsTest.assertGeoLocations(it.next(), new LatLong(64, 11), new LatLong(64.1, 11.1));
+			Collection<GeoLocations> geoLocations = persistence.loadLocations(getClass().getResourceAsStream("geoLocations.json"));
+			testGeoLocationsDotJson(geoLocations);
 		} catch (Exception e) {
 			Assert.fail(e.getMessage());
 		}
 	}
+	
+	public static Collection<GeoLocations> createGeoLocationsDotJson() {
+		return Arrays.asList(
+			new GeoLocations("1", new LatLong(63, 10), new LatLong(63.1, 10.1)),
+			new GeoLocations("2", new LatLong(64, 11), new LatLong(64.1, 11.1))
+		);
+	}
+	
+	public static void testGeoLocationsDotJson(Collection<GeoLocations> geoLocations) {
+		Assert.assertEquals(2, geoLocations.size());
+		Iterator<GeoLocations> it = geoLocations.iterator();
+		GeoLocationsTest.assertGeoLocations(it.next(), new LatLong(63, 10), new LatLong(63.1, 10.1));
+		GeoLocationsTest.assertGeoLocations(it.next(), new LatLong(64, 11), new LatLong(64.1, 11.1));
+	}
 
 	@Test
 	public void testSaveLocations() {
-		Collection<GeoLocations> geoLocations = new ArrayList<GeoLocations>();
-		geoLocations.add(new GeoLocations("1", new LatLong(63, 10), new LatLong(63.1, 10.1)));
-		geoLocations.add(new GeoLocations("2", new LatLong(64, 11), new LatLong(64.1, 11.1)));
+		Collection<GeoLocations> geoLocations = createGeoLocationsDotJson();
 		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
 		try {
 			persistence.saveLocations(geoLocations, outputStream);
 			outputStream.close();
-			testLoadLocations(new ByteArrayInputStream(outputStream.toByteArray()));
+			Collection<GeoLocations> geoLocations2 = persistence.loadLocations(new ByteArrayInputStream(outputStream.toByteArray()));
+			testGeoLocationsDotJson(geoLocations2);
 		} catch (Exception e) {
 			Assert.fail(e.getMessage());
 		}
