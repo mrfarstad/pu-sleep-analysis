9a0c51d13b13895c6d668c7bcaedd5064b5ab6cb
Martin Farstad <farstad2k@gmail.com>
2018-02-26 00:34:58 +0100

    Add connectors

--- tdt4140-gr1816/app.api/src/main/java/tdt4140/gr1816/app/api/resolvers/Query.java
+++ tdt4140-gr1816/app.api/src/main/java/tdt4140/gr1816/app/api/resolvers/Query.java
@@ -1,19 +1,32 @@
 package tdt4140.gr1816.app.api.resolvers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
 
 import graphql.schema.DataFetchingEnvironment;
 import tdt4140.gr1816.app.api.CharacterRepository;
+import tdt4140.gr1816.app.api.LinkRepository;
+import tdt4140.gr1816.app.api.types.Character;
 import tdt4140.gr1816.app.api.types.Droid;
 import tdt4140.gr1816.app.api.types.Episode;
-import tdt4140.gr1816.app.api.types.Character;
 import tdt4140.gr1816.app.api.types.Human;
+import tdt4140.gr1816.app.api.types.Link;
 
 @Component
 public class Query implements GraphQLQueryResolver {
+	
+    private static final LinkRepository linkRepository;
+
+    static {
+        MongoDatabase mongo = new MongoClient().getDatabase("gruppe16");
+        linkRepository = new LinkRepository(mongo.getCollection("links"));
+    }
 
 	@Autowired
 	private CharacterRepository characterRepository;
@@ -38,4 +51,9 @@ public class Query implements GraphQLQueryResolver {
 	public Character character(String id) {
 		return characterRepository.getCharacters().get(id);
 	}
+
+	public List<Link> allLinks() {
+		return linkRepository.getAllLinks();
+	}
+
 }
