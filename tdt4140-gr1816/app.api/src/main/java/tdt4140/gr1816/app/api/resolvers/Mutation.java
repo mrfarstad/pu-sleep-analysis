8a926100b7c5f2906a77844be8ddbe3d1177abe0
Odin Ugedal <odin@ugedal.com>
2018-02-26 01:05:10 +0100

    Fix mutations

--- tdt4140-gr1816/app.api/src/main/java/tdt4140/gr1816/app/api/resolvers/Mutation.java
+++ tdt4140-gr1816/app.api/src/main/java/tdt4140/gr1816/app/api/resolvers/Mutation.java
@@ -1,15 +1,21 @@
 package tdt4140.gr1816.app.api.resolvers;
 
 import tdt4140.gr1816.app.api.LinkRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import org.springframework.stereotype.Component;
 import tdt4140.gr1816.app.api.types.Link;
 import com.coxautodev.graphql.tools.GraphQLMutationResolver;
 
+@Component
 public class Mutation implements GraphQLMutationResolver {
     
-    private final LinkRepository linkRepository;
+    private static final LinkRepository linkRepository;
 
-    public Mutation(LinkRepository linkRepository) {
-        this.linkRepository = linkRepository;
+    static {
+        MongoDatabase mongo = new MongoClient().getDatabase("gruppe16");
+        linkRepository = new LinkRepository(mongo.getCollection("links"));
     }
     
     public Link createLink(String url, String description) {
