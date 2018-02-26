9e7febda1b387e318786cd562e77a9997c3133cf
Sondre <sondre.grav@gmail.com>
2018-02-26 13:05:39 +0100

    Min side FXML og Controller klasse

---
+++ tdt4140-gr1816/app.ui/src/main/java/tdt4140/gr1816/app/ui/MinSideController.java
@@ -0,0 +1,15 @@
+package tdt4140.gr1816.app.ui;
+
+import javafx.scene.control.Button;
+
+public class MinSideController{
+
+	public Button dataKnapp;
+	
+	public void handleDataKnapp() {
+		dataKnapp.setText("Skru av");
+	}
+	
+
+	
+}
