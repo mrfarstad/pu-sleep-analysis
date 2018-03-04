ff012a6d564951238f414daa9b4c80d395622cab
Odin Ugedal <odin@ugedal.com>
2018-03-04 22:04:17 +0100

    Add initial support for data access
    Still no mutation

--- tdt4140-gr1816/app.api/src/main/java/tdt4140/gr1816/app/api/resolvers/Query.java
+++ tdt4140-gr1816/app.api/src/main/java/tdt4140/gr1816/app/api/resolvers/Query.java
@@ -1,11 +1,18 @@
 package tdt4140.gr1816.app.api.resolvers;
 
-import com.coxautodev.graphql.tools.GraphQLRootResolver;
-import graphql.schema.DataFetchingEnvironment;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import com.coxautodev.graphql.tools.GraphQLRootResolver;
+
+import graphql.schema.DataFetchingEnvironment;
+import tdt4140.gr1816.app.api.CharacterRepository;
+import tdt4140.gr1816.app.api.DataAccessRequestRepository;
 import tdt4140.gr1816.app.api.LinkRepository;
 import tdt4140.gr1816.app.api.UserRepository;
 import tdt4140.gr1816.app.api.auth.AuthContext;
+import tdt4140.gr1816.app.api.types.DataAccessRequest;
 import tdt4140.gr1816.app.api.types.Link;
 import tdt4140.gr1816.app.api.types.User;
 
@@ -13,22 +20,76 @@ public class Query implements GraphQLRootResolver {
 
   private final LinkRepository linkRepository;
   private final UserRepository userRepository;
+  private final DataAccessRequestRepository dataAccessRequestRepository;
 
-  public Query(LinkRepository linkRepository, UserRepository userRepository) {
+  public Query(
+      LinkRepository linkRepository,
+      UserRepository userRepository,
+      DataAccessRequestRepository dataAccessRequestRepository) {
     this.linkRepository = linkRepository;
     this.userRepository = userRepository;
+    this.dataAccessRequestRepository = dataAccessRequestRepository;
   }
 
   public List<Link> allLinks() {
     return linkRepository.getAllLinks();
   }
 
-  public List<User> allUsers() {
-    return userRepository.getAllUsers();
+  public List<DataAccessRequest> allDataAccessRequests(DataFetchingEnvironment env) {
+    AuthContext context = env.getContext();
+    User user = context.getUser();
+    if (user == null) {
+      return new ArrayList<DataAccessRequest>();
+    }
+    return dataAccessRequestRepository.getAllDataAccessRequests();
   }
 
   public User viewer(DataFetchingEnvironment env) {
     AuthContext context = env.getContext();
     return context.getUser();
   }
+
+  public List<User> allUsers() {
+    return userRepository.getAllUsers();
+  }
+
+	private List<DataAccessRequest> allDataAccessRequests(DataFetchingEnvironment env) {
+		AuthContext context = env.getContext();
+		User user = context.getUser();
+		if (user == null) {
+			return new ArrayList<DataAccessRequest>();
+		}
+		return dataAccessRequestRepository.getAllDataAccessRequests();
+	}
+
+	public List<DataAccessRequest> myDataAccessRequests(DataFetchingEnvironment env) {
+		AuthContext context = env.getContext();
+		User user = context.getUser();
+		if (user == null) {
+			return new ArrayList<DataAccessRequest>();
+		}
+		return allDataAccessRequests(env).stream().filter(request -> request.getRequestedById().equals(user.getId()))
+				.collect(Collectors.toList());
+	}
+
+	public List<DataAccessRequest> dataAccessRequestsForMe(DataFetchingEnvironment env) {
+		AuthContext context = env.getContext();
+		User user = context.getUser();
+		if (user == null) {
+			return new ArrayList<DataAccessRequest>();
+		}
+		return allDataAccessRequests(env)
+				.stream()
+				.filter(request -> request.getDataOwnerId().equals(user.getId()))
+				.collect(Collectors.toList());
+	}
+
+	public User viewer(DataFetchingEnvironment env) {
+		AuthContext context = env.getContext();
+		return context.getUser();
+	}
+
+	public List<User> allUsers() {
+		return userRepository.getAllUsers();
+	}
 }
