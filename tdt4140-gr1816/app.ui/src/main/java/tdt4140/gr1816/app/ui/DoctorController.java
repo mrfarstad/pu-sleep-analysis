c4b69a7f427d2e8afd759624ec3b20332edf51ed
Your Name <ololololole@mail.com>
2018-03-07 15:16:59 +0100

    Improve DoctorGUI

--- tdt4140-gr1816/app.ui/src/main/java/tdt4140/gr1816/app/ui/DoctorController.java
+++ tdt4140-gr1816/app.ui/src/main/java/tdt4140/gr1816/app/ui/DoctorController.java
@@ -1,5 +1,9 @@
 package tdt4140.gr1816.app.ui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class DoctorController {
 
 	@FXML
@@ -8,4 +12,11 @@ public class DoctorController {
 	@FXML
 	private TextField requestTextField;
 	
+	public void handleRequestButton() {
+		System.out.println("Data requested");
+		
+		System.out.println(requestTextField.getText());
+		requestTextField.setText("HALLO");
+		
+	}
 }
