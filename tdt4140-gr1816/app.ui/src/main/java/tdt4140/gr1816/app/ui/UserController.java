af156b81f953f34f57678ad8ef12297e66f5de33
Sondre <sondre.grav@gmail.com>
2018-03-07 13:51:18 +0100

    Added some API functionality

--- tdt4140-gr1816/app.ui/src/main/java/tdt4140/gr1816/app/ui/UserController.java
+++ tdt4140-gr1816/app.ui/src/main/java/tdt4140/gr1816/app/ui/UserController.java
@@ -1,16 +1,17 @@
 package tdt4140.gr1816.app.ui;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
-import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import tdt4140.gr1816.app.core.DataAccessRequest;
+import tdt4140.gr1816.app.core.User;
 import tdt4140.gr1816.app.core.UserDataFetch;
 
 public class UserController implements Initializable {
@@ -25,11 +26,29 @@ public class UserController implements Initializable {
 	private Button removeDoctorButton;
 	
 	@FXML
+	private Text nameText;
+	
+	@FXML
+	private Text ageText;
+	
+	@FXML
+	private Text genderText;
+	
+	@FXML
 	private ListView<String> doctorsListView;
 	
+	@FXML
+	private ListView<String> doctorRequestListView;
+	
+	private User user;
+	private UserDataFetch userDataFetch;
+	
+	
+	private List<DataAccessRequest> accessRequestList;
 	
 	private boolean dataGatheringOn;
-	ObservableList<String> listViewItems;
+	ObservableList<String> doctorsListViewItems;
+	ObservableList<String> doctorRequestListViewItems;
 
 	public void handleDataButton() {
 		if (dataGatheringOn) {
@@ -46,11 +65,12 @@ public class UserController implements Initializable {
 	}
 	
 	public void handleRemoveDoctorButton() {
-		listViewItems.remove(doctorsListView.getSelectionModel().getSelectedItem());
+		doctorsListViewItems.remove(doctorsListView.getSelectionModel().getSelectedItem());
 	}
 	
 	public void handleAcceptDoctorButton() {
-		acceptDoctorButton.setVisible(false);
+		doctorsListViewItems.add(doctorRequestListView.getSelectionModel().getSelectedItem());
+		doctorRequestListViewItems.remove(doctorRequestListView.getSelectionModel().getSelectedItem());
 	}
 	
 	public void turnOffDataGathering() {
@@ -63,8 +83,33 @@ public class UserController implements Initializable {
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
+		UserDataFetch.signIn();
+		user = UserDataFetch.getCurrentUser();
+		accessRequestList = UserDataFetch.getAccessRequestsToUser();
 		
-		//SET INITIAL VALUE OF DATA BUTTON
+		setProfileValues();
+		
+		setInitialDataButtonValue();
+			
+		setDoctorsListViewItems();
+		
+		setDoctorRequestListViewItems();
+		
+	}
+	
+	public void setProfileValues() {
+		
+		String name = user.getUsername();
+		Integer age = user.getAge();
+		String gender = user.getGender();
+		
+		nameText.setText(name);
+		ageText.setText(age.toString());
+		genderText.setText(gender);
+				
+	}
+	
+	public void setInitialDataButtonValue() {
 		this.dataGatheringOn = true;
 		
 		if (dataGatheringOn) {
@@ -72,15 +117,25 @@ public class UserController implements Initializable {
 		}
 		else {
 			dataButton.setText("Turn on");
-		}		
-		
-		//SET LISTVIEW ITEMS
-		listViewItems = doctorsListView.getItems();
-		listViewItems.add("Doctor 1");
-		listViewItems.add("Doctor 2");
-		listViewItems.add("Doctor 3");
-		listViewItems.add("Doctor 4");
-		
-		
+		}	
+	}
+	
+	public void setListViews() {
+		List<DataAccessRequest> dataAccessRequest = UserDataFetch.getAccessRequestsToUser();
+	}
+	
+	public void setDoctorsListViewItems() {
+		doctorsListViewItems = doctorsListView.getItems();
+		doctorsListViewItems.add("Doctor 1");
+		doctorsListViewItems.add("Doctor 2");
+		doctorsListViewItems.add("Doctor 3");
+		doctorsListViewItems.add("Doctor 4");
+	}
+	
+	public void setDoctorRequestListViewItems() {
+		doctorRequestListViewItems = doctorRequestListView.getItems();
+		doctorRequestListViewItems.add("Doctor 5");
+		doctorRequestListViewItems.add("Doctor 6");
+		doctorRequestListViewItems.add("Doctor 7");
 	}
 }
