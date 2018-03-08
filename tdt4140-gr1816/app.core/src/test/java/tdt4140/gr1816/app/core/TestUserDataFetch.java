169d5e9733d3917a52bead88899d19ea8cc7a256
Mathias Wahl <ma10asw@gmail.com>
2018-03-08 12:19:08 +0100

    Add tests for createUser and deleteUser. Also, createUser now returns User

--- tdt4140-gr1816/app.core/src/test/java/tdt4140/gr1816/app/core/TestUserDataFetch.java
+++ tdt4140-gr1816/app.core/src/test/java/tdt4140/gr1816/app/core/TestUserDataFetch.java
@@ -8,6 +8,9 @@ import org.junit.Test;
 
 public class TestUserDataFetch {
 
+  private static String createUserResponse =
+      "{\"data\":{\"createUser\":{\"id\":\"5aa117f0c40c0b0451c261c2\",\"username\":\"test\",\"isDoctor\":true,\"gender\":\"male\",\"age\":33}}}";
+  private static String deleteUserResponse = "{\"data\":{\"deleteUser\":true}}";
   private static String getAllUsersResponse =
       "{\"data\":{\"allUsers\":[{\"id\":\"5a9e8503c13edf22f93825e7\",\"username\":\"test\",\"isDoctor\":true,\"gender\":\"male\",\"age\":22},{\"id\":\"5a9e85cac13edf22f93825e8\",\"username\":\"test\",\"isDoctor\":true,\"gender\":\"male\",\"age\":22}]}}";
   private static String getCurrentUserResponse =
@@ -21,6 +24,34 @@ public class TestUserDataFetch {
   private DataGetter test = mock(DataGetter.class);
 
   private UserDataFetch userDataFetch = new UserDataFetch(test);
+
+  @Test
+  public void testCreateUserQuery() {
+    when(test.getData(userDataFetch.createUserQuery, null)).thenReturn(createUserResponse);
+
+    assertEquals(test.getData(userDataFetch.createUserQuery, null), createUserResponse);
+
+    // CreateUser should return a user:
+    User user = userDataFetch.createUser();
+    assertTrue(user instanceof User);
+    assertNotNull(user);
+    assertTrue(user.getUsername().equals("test"));
+    assertTrue(user.isDoctor());
+    assertTrue(user.getGender().equals("male"));
+    assertTrue(user.getAge() == 33);
+  }
+
+  @Test
+  public void testDeleteUserQuery() {
+    when(test.getData(userDataFetch.deleteUserQuery, null)).thenReturn(deleteUserResponse);
+
+    assertEquals(test.getData(userDataFetch.deleteUserQuery, null), deleteUserResponse);
+
+    // UserDataFetch should return a boolean (success)
+    boolean success = userDataFetch.deleteUser();
+    assertNotNull(success);
+    assertTrue(success);
+  }
 
   @Test
   public void testGetAllUsersQuery() {
