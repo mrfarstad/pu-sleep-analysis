ad0b12a02e458fe77f93c59207c75142c16478ae
Anders <andersogranaas@gmail.com>
2018-03-13 22:31:27 +0100

    Fix handleCreateUser with correct userDataFetch and some more minor fixes to the method

--- tdt4140-gr1816/app.ui/src/main/java/tdt4140/gr1816/app/ui/CreateNewUserController.java
+++ tdt4140-gr1816/app.ui/src/main/java/tdt4140/gr1816/app/ui/CreateNewUserController.java
@@ -1,12 +1,15 @@
 package tdt4140.gr1816.app.ui;
 
+import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
-
+import javafx.stage.Stage;
+import javafx.stage.Window;
 import tdt4140.gr1816.app.core.*;
 
 public class CreateNewUserController {
@@ -20,23 +23,24 @@ public class CreateNewUserController {
   @FXML private RadioButton isDoctorRadioButton;
   @FXML private Button createUserButton;
 
-  // Uses username, password, isDoctor, age and gender
-  public void handleCreateUser() { 
-    UserDataFetch udf = new UserDataFetch(new DataGetter());
+  // Uses username, password, isDoctor, age and gender to create a new user using createUser in
+  // userDataFetch in FxApp
+  public void handleCreateUser() throws IOException {
     String username = signupUsernameField.getText();
     String password = signupPasswordField.getText();
-    boolean isDoctor = isDoctorRadioButton.isPressed();
-    String gender;
-    if (femaleRadioButton.isPressed()) {
-    	gender = "Female";
-    } else if (maleRadioButton.isPressed()) {
-    	gender = "Male";
-    } else {
-    	gender = "Other";
-    }
+    boolean isDoctor = isDoctorRadioButton.isSelected();
+    RadioButton gender = (RadioButton) femaleRadioButton.getToggleGroup().getSelectedToggle();
     int age = Integer.parseInt(ageField.getText());
-    
-    
-    udf.createUser(username, password, isDoctor, gender , age);
+
+    FxApp.userDataFetch.createUser(username, password, isDoctor, gender.getText(), age);
+
+    // Open up login-screen and close this window
+    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("FxApp.fxml"));
+    Parent root1 = (Parent) fxmlLoader.load();
+    Stage createStage = new Stage();
+    createStage.setScene(new Scene(root1));
+    createStage.show();
+    Window newUserStage = signupUsernameField.getScene().getWindow();
+    newUserStage.hide();
   }
 }
