372b1ab8740bf0394c81d340e3c65bff4451e9e9
Odin Ugedal <odin@ugedal.com>
2018-03-20 22:33:23 +0100

    Disable duplicate date values for data
    This updates old entries if data for the given date & user is already in
    the database. This also makes it impossible to make two users with the
    same username.

--- tdt4140-gr1816/app.api/src/main/java/tdt4140/gr1816/app/api/StepsDataRepository.java
+++ tdt4140-gr1816/app.api/src/main/java/tdt4140/gr1816/app/api/StepsDataRepository.java
@@ -3,10 +3,12 @@ package tdt4140.gr1816.app.api;
 import static com.mongodb.client.model.Filters.eq;
 
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
 import com.mongodb.client.result.DeleteResult;
 import java.util.ArrayList;
 import java.util.List;
 import org.bson.Document;
+import org.bson.conversions.Bson;
 import org.bson.types.ObjectId;
 import tdt4140.gr1816.app.api.types.StepsData;
 
@@ -36,7 +38,18 @@ public class StepsDataRepository {
     doc.append("user", stepsData.getUserId());
     doc.append("date", stepsData.getDate());
     doc.append("steps", stepsData.getSteps());
-    stepsDoc.insertOne(doc);
+
+    Bson filter =
+        Filters.and(
+            Filters.eq("date", stepsData.getDate()), Filters.eq("user", stepsData.getUserId()));
+    Document old = stepsDoc.find(filter).first();
+    if (old != null) {
+      stepsDoc.updateOne(
+          eq("_id", new ObjectId(old.get("_id").toString())), new Document("$set", doc));
+      doc.append("_id", old.get("_id").toString());
+    } else {
+      stepsDoc.insertOne(doc);
+    }
     return new StepsData(
         doc.get("_id").toString(),
         stepsData.getUserId(),
