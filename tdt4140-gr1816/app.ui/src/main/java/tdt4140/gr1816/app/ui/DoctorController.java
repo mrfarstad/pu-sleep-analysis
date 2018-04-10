df4dd929092d9cc4b85078c0d3199d505dccdf42
Sondre Skjåstad <sondre.grav@gmail.com>
2018-04-10 13:45:36 +0200

    Rebased branch and made messaging work

--- tdt4140-gr1816/app.ui/src/main/java/tdt4140/gr1816/app/ui/DoctorController.java
+++ tdt4140-gr1816/app.ui/src/main/java/tdt4140/gr1816/app/ui/DoctorController.java
@@ -17,12 +17,12 @@ import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
-import javafx.scene.control.TextField;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import javafx.util.Duration;
 import tdt4140.gr1816.app.core.*;
@@ -34,7 +34,7 @@ public class DoctorController implements Initializable {
   @FXML private Button showDataButton;
 
   @FXML private Button showMessageButton;
-  
+
   @FXML private Button sendMessageButton;
 
   @FXML private Text nameText;
@@ -44,21 +44,21 @@ public class DoctorController implements Initializable {
   @FXML private Text ageText;
 
   @FXML private Text requestFeedbackText;
-  
+
   @FXML private Text subjectText;
-  
+
   @FXML private Text toText;
-  
+
   @FXML private Text fromText;
-  
+
   @FXML private TextField subjectTextField;
-  
+
   @FXML private TextArea sendMessageTextArea;
-  
+
   @FXML private TextArea messageTextArea;
-  
+
   @FXML private ChoiceBox<User> toChoiceBox;
-  
+
   @FXML private Label sentLabel;
 
   @FXML private Tab dataTab;
@@ -74,7 +74,7 @@ public class DoctorController implements Initializable {
   @FXML private TextField requestUserTextField;
 
   @FXML private ListView<DataAccessRequest> patientListView;
-  
+
   @FXML private ListView<Message> messagesListView;
 
   ObservableList<DataAccessRequest> patientListViewItems;
@@ -227,21 +227,24 @@ public class DoctorController implements Initializable {
     stepBarChart.setVisible(false);
     sleepBarChart.setVisible(false);
   }
-  
+
   public void handleMessagesListViewClicked() {
-	  Message message = messagesListView.getSelectionModel().getSelectedItem();
-	  subjectText.setText(message.getSubject());
-	  fromText.setText(message.getFrom());
-	  toText.setText(message.getTo());
-	  messageTextArea.setText(message.getMessage());
+    Message message = messagesListView.getSelectionModel().getSelectedItem();
+    if (message != null) {
+      subjectText.setText(message.getSubject());
+      fromText.setText(message.getFrom().getUsername());
+      toText.setText(message.getTo().getUsername());
+      messageTextArea.setText(message.getMessage());
+    }
   }
 
-  
   public void handleSendMessageButton() {
-	  String subject = subjectTextField.getText();
-	  User from = user;
-	  User to = toChoiceBox.getValue();
-	  String message = sendMessageTextArea.getText();
+    String subject = subjectTextField.getText();
+    String to = toChoiceBox.getValue().getId();
+    String message = sendMessageTextArea.getText();
+
+    userDataFetch.createMessage(to, subject, message);
+    updateMessagesListViewItems();
   }
 
   @Override
@@ -278,21 +281,19 @@ public class DoctorController implements Initializable {
         .filter(request -> !patientListViewItems.contains(request))
         .forEach(request -> patientListViewItems.add(request));
   }
-  
-  public void updateMessagesListViewItems() {
-	  messagesListViewItems = messagesListView.getItems();
-	  messagesListViewItems.clear();
-	  List<Message> messages = userDataFetch.messagesForMe();
-	  messages.stream().forEach(request -> messagesListViewItems.add(message));
-  }
 
+  public void updateMessagesListViewItems() {
+    messagesListViewItems = messagesListView.getItems();
+    messagesListViewItems.clear();
+    List<Message> messages = userDataFetch.messagesForMe();
+    messages.stream().forEach(message -> messagesListViewItems.add(message));
+  }
 
   public void setPatientChoiceBoxes() {
     acceptedPatientList.clear();
     List<DataAccessRequest> requests = userDataFetch.getAccessRequestsByDoctor();
     requests
         .stream()
-        .filter(request -> !acceptedPatientList.contains(request.getDataOwner()))
         .filter(request -> request.getStatusAsString().equals("ACCEPTED"))
         .forEach(request -> acceptedPatientList.add(request.getDataOwner()));
     patientChoiceBox.setItems(acceptedPatientList);
